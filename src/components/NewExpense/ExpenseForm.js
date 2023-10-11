b4c10bbb10cab7398c0e3c7a58e116f02943f0fb
Yuyuu0216 <zoe106429@gmail.com>
2023-10-11 15:00:29 +0800

    no message

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -43,8 +43,20 @@ function ExpenseForm() {
         // })
     };
 
+    const submitHandler = (event) => {
+      event.preventDefault();
+
+      const expenseData = {
+        title: enteredTitle,
+        Amount: enteredAmount,
+        Date: new Date(enteredDate)
+      }
+
+      console.log(expenseData);
+    };
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
